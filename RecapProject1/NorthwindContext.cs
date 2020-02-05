276e08d8d43bc664d4a7687b5624f835610de208
Umut Yüksel <akilian092@gmail.com>
2020-02-05 18:19:00 +0300

    Proje dosyası ekle.

---
+++ RecapProject1/NorthwindContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecapProject1.Entities;
+
+namespace RecapProject1
+{
+    //EntityFramework u kullanabilmek için Context sınıfının
+    //DbContext en oluşturulması gerek
+    public class NorthwindContext:DbContext
+    {
+        //Burada nesnelerin veritabanı ile bağlantısı gerçekleştiriliyor
+        //Veritabanında tabloya karşılık gelen nesneye ihtiyaç var
+
+        //Benim bir dbsetim var veritabanındaki Products alanına karşılık geliyor anlamında
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
